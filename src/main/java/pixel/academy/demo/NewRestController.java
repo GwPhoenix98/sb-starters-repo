664cce902bd8f803a87b6e68c4a5de387f574123
Ion Schimbator <ionschimbator98@gmail.com>
2025-12-27 11:00:59 +0200

    Add rest controller

---
+++ src/main/java/pixel/academy/demo/NewRestController.java
@@ -0,0 +1,14 @@
+package pixel.academy.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewRestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Republic of Moldova";
+    }
+
+}
