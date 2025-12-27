9f2b43d6e68bfcd38262b24b5ed1e7b320d9e36f
Ion Schimbator <ionschimbator98@gmail.com>
2025-12-27 10:55:09 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/demo/SbStartersApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStartersApplication.class, args);
+	}
+
+}
